0bdef352cab20a2176f3cabe1474f0435d6b4a29
EZForever <34133756+EZForever@users.noreply.github.com>
2019-09-04 20:09:18 +0800

    [base24] add real base24 algorithm used in Microsoft product key generation

--- base24.py
+++ base24.py
@@ -18,3 +18,16 @@ def b24decode(data):
             hi = -1
     return dec
 
+def b24Keygen(data):
+    ''' The actual algorithm used in Windows product key generation. '''
+    assert len(data) * 8 >= 114, 'data must be at least 144 bits in length'
+    num = int.from_bytes(data, byteorder = 'little')
+    enc = b''
+    for i in range(29):
+        if i % 6 == 5:
+            enc += b'-'
+        else:
+            enc += CHARMAP[num % 24]
+            num //= 24
+    return enc
+
