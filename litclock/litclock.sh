2bb7a3ddf27838d0481b36812e1f7a59ea18d054
EZForever <34133756+EZForever@users.noreply.github.com>
2023-07-15 19:11:21 +0800

    [litclock] Many improvements - Updated database - Correctly parses CSV quotes - Strips leading/trailing spaces in each field - Time string is now compared case-insensitively

--- litclock/litclock.sh
+++ litclock/litclock.sh
@@ -4,7 +4,7 @@
 
 function litclock {
     local tm=${1:-$(date +%H:%M)}
-    local db=${2:-$(dirname ${BASH_SOURCE[0]})"/litclock_annotated_improved.csv"}
+    local db=${2:-$(dirname ${BASH_SOURCE[0]})/litclock_annotated_improved.csv}
     
     local IFS='|'
     local entry=($(grep "^$tm" "$db" | shuf -n 1))
@@ -13,13 +13,34 @@ function litclock {
         return 1
     fi
     
-    local quote=${entry[2]//${entry[1]}/$'\e[32m'${entry[1]}$'\e[0m'}
-    local cite="-- \"${entry[3]}\", ${entry[4]}"
+    local i
+    for i in ${!entry[@]}; do
+        local item="${entry[$i]}"
+        if [[ $item == '"'*'"' ]]; then
+            item=${item:1:${#item}-2}
+            item=${item//\"\"/\"}
+        fi
+        item=${item## }
+        item=${item%% }
+        entry[$i]=$item
+    done
     
-    printf "%s\n\e[1;30m%80s\e[0m\n" "$quote" "$cite"
+    local timestr=${entry[1]}
+    local quote=${entry[2]}
+    local reference=$'\e[3m'"${entry[3]}"$'\e[23m'
+    local author=${entry[4]}
+    local cite="-- $reference, $author"
+    
+    # NOTE: Do not use ${x%%} and ${x#} since they do not respect nocasematch
+    shopt -s nocasematch
+    local quote_l=${quote/$timestr*}
+    quote=$quote_l$'\e[32m'${quote:${#quote_l}:${#timestr}}$'\e[0m'${quote:${#quote_l}+${#timestr}}
+    shopt -u nocasematch
+    
+    # NOTE: That additional 9 is from ANSI sequences in $cite
+    printf "%s\n\e[1;30m%*s\e[0m\n" "$quote" $((80 + 9)) "$cite"
     return 0
 }
 
 litclock $*
-echo
 
