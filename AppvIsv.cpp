234a82d216ddc02d4f27db50c33f9c06d27f530f
EZForever <34133756+EZForever@users.noreply.github.com>
2024-05-30 21:25:27 +0800

    AppvIsv: Prototype of RequestUnhookedFunctionList

--- AppvIsv.cpp
+++ AppvIsv.cpp
@@ -1,4 +1,4 @@
-﻿#define WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #include <stdio.h>
 
@@ -15,12 +15,32 @@
 
 // AppvIsvSubsystems64.dll is symlinked into Office root in various places
 
+// NOTE: ETW provider ID AppVClientDbg = {9CC69D1C-7917-4ACD-8066-6BF8B63E551B}; ClientEventLogMessages.man
+
+typedef void (CALLBACK * SubsystemUnhookedFunctionListCallback_t)(SIZE_T uiCount, FARPROC* ppfnFunctionPointers, LPCSTR* ppszFunctionNames); // Called in RequestUnhookedFunctionList
+typedef void (CALLBACK * SubsystemUnloadCallback_t)(); // Called in AppvIsvSubsystems64!DllMain(DLL_PROCESS_DETACH)
+
 typedef int (WINAPI * APIExportForDetours_t)(); // Ordinal #1, Do nothing but return 1
-typedef BOOLEAN (WINAPI * RequestUnhookedFunctionList_t)(FARPROC pfnUnkCallback1, FARPROC pfnUnkCallback2); // TODO: Type and usage
+typedef BOOLEAN (WINAPI * RequestUnhookedFunctionList_t)(SubsystemUnhookedFunctionListCallback_t pfnUnhookedFunctionListCallback, SubsystemUnloadCallback_t pfnUnloadCallback); // Could only be called once; both callbacks must be in the same module as caller
 typedef void (WINAPI * VirtualizeCurrentThread_t)(BOOL bEnable);
 typedef BOOLEAN (WINAPI * CurrentThreadIsVirtualized_t)();
 typedef void (WINAPI * VirtualizeCurrentProcess_t)(BOOL bEnable); // bEnable must be TRUE
 typedef BOOLEAN (WINAPI * IsProcessHooked_t)();
+
+// NOTE: The pointers are only valid during the callback
+static void CALLBACK UnhookedFunctionListCallback(SIZE_T uiCount, FARPROC* ppfnFunctionPointers, LPCSTR* ppszFunctionNames)
+{
+	fprintf(stderr, "UnhookedFunctionListCallback(%zu)\n", uiCount);
+	for (SIZE_T i = 0; i < uiCount; i++)
+	{
+		fprintf(stderr, "%p\t%s\n", ppfnFunctionPointers[i], ppszFunctionNames[i]);
+	}
+}
+
+static void CALLBACK UnloadCallback()
+{
+	fprintf(stderr, "UnloadCallback()\n");
+}
 
 int main()
 {
@@ -43,13 +63,17 @@ int main()
 	API(VirtualizeCurrentProcess);
 	API(IsProcessHooked);
 
+#undef API
+
 	printf("IsProcessHooked() = %hhu\n", IsProcessHooked());
 	printf("CurrentThreadIsVirtualized() = %hhu\n", CurrentThreadIsVirtualized());
+	printf("RequestUnhookedFunctionList() = %hhu\n", RequestUnhookedFunctionList(UnhookedFunctionListCallback, UnloadCallback));
 
 	VirtualizeCurrentProcess(TRUE);
 
 	printf("IsProcessHooked() = %hhu\n", IsProcessHooked());
 	printf("CurrentThreadIsVirtualized() = %hhu\n", CurrentThreadIsVirtualized());
+	printf("RequestUnhookedFunctionList() = %hhu\n", RequestUnhookedFunctionList(UnhookedFunctionListCallback, UnloadCallback));
 
 	// This DLL is from Office VBA and is in App-V VFS
 	HMODULE fm20 = LoadLibraryW(L"FM20.DLL");
