51c7f7dd70f8b25970d0f1d3a0d64d164d74bcb1
EZForever <34133756+EZForever@users.noreply.github.com>
2024-11-26 23:23:47 +0800

    winsta: Initial attempt at manually creating sessions

---
+++ winsta.cpp
@@ -0,0 +1,35 @@
+﻿#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#include <objbase.h>
+
+#include <stdio.h>
+
+#pragma comment(lib, "winsta.lib")
+
+extern "C"
+{
+	BOOL WinStationPreCreateGlassReplacementSession(LPCWSTR lpwszInitialCommand, DWORD dwConnectTimeout, DWORD* lpdwSessionId); // dwReconnectTargetId == -1
+	BOOL WinStationPreCreateGlassReplacementSessionEx(LPCWSTR lpwszInitialCommand, DWORD dwReconnectTargetId, DWORD dwConnectTimeout, DWORD* lpdwSessionId);
+	BOOL WinStationTerminateGlassReplacementSession(DWORD dwSessionId);
+}
+
+int main()
+{
+	DWORD dwSessionId = 0;
+	if (!WinStationPreCreateGlassReplacementSession(L"%SYSTEMROOT%\\System32\\WinLogon.exe -SpecialSession", 0, &dwSessionId))
+	{
+		fwprintf(stderr, L"WinStationPreCreateGlassReplacementSession(), GetLastError() = %u\n", GetLastError());
+		return 1;
+	}
+
+	wprintf(L"dwSessionId = %u\n", dwSessionId);
+	system("pause");
+
+	if (!WinStationTerminateGlassReplacementSession(dwSessionId))
+	{
+		fwprintf(stderr, L"WinStationTerminateGlassReplacementSession(), GetLastError() = %u\n", GetLastError());
+		return 1;
+	}
+
+	return 0;
+}
